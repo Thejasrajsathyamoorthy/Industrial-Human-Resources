b3bd55b04d79d9c84416eeed185d3f90c7150e98
Thejas_Raj_Sathiyamoorthy <thejasrajsathyamoorthy@gmail.com>
2024-12-11 20:47:16 +0530

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is behind 'origin/main' by 7 commits, and can be fast-forwarded.
      (use "git pull" to update your local branch)
    
    Changes to be committed:
    	new file:   Analysis.png
    	new file:   EDA_District_wise_df.csv
    	new file:   EDA_Statewise_df.csv
    	modified:   HR_1.ipynb
    	modified:   HR_1.py
    	modified:   I_HR Presentation.pptx
    	modified:   Industrial_Human_Resouces.pkl
    	new file:   Intro.png
    	new file:   Joblib.png
    	new file:   Matplotlib.png
    	modified:   NLP_df.csv
    	new file:   NumPy_logo_2020.svg.png
    	new file:   Pandas_logo.svg.png
    	new file:   Plotly.jpg
    	new file:   Scikit-learn.png
    	new file:   Seaborn.jpg
    	new file:   State_Name_code.csv
    	new file:   Visualization_1.png
    	new file:   Visualization_2.png
    	new file:   Visualization_3.png
    	new file:   label_encoder_classes.npy
    	new file:   vectorizer.pkl
    
    Changes not staged for commit:
    	deleted:    EDA_df.csv
    	modified:   HR_1.py
    	deleted:    Joblib.png
    	deleted:    Seaborn.jpg
    
    Untracked files:
    	Industrial Human Resources Geo-visualization.html
    	Industrial Human Resources Geo-visualization_files/
    	Seaborn.png

--- HR_1.py
+++ HR_1.py
@@ -10,21 +10,18 @@ import joblib
 I_HR_model = joblib.load("Industrial_Human_Resouces.pkl")
 
 nlp_df = pd.read_csv("NLP_df.csv")
-eda_df = pd.read_csv("EDA_df.csv")
-
+state_name_code = pd.read_csv("State_Name_code.csv")
 
 # Prediction of Industry Groups
 
 def preprocess_input(state_code, district_code, division, group, class_):
-    # Replace with your mapping logic if required
     return pd.DataFrame({
         "state_code": [state_code],
         "district_code": [district_code],
         "division": [division],
         "group": [group],
         "class": [class_]
-    })
-
+                })
 
 
 # Visualisation
@@ -43,12 +40,12 @@ def plot_top_industries(df, group_col, value_col, top_n, title, color ):
 
 
 
-def plot_workers_distribution(data):
+def plot_workers_distribution_state(data):
     geo_data = data.groupby('state_code')['total_workers'].sum().reset_index()
 
     workers_fig, ax = plt.subplots(figsize=(12, 8))
     sns.barplot(x='state_code', y='total_workers', data=geo_data, ax=ax, palette="viridis")
-    ax.set_title("Workers Distribution by State")
+    ax.set_title("Workers Population State-wise")
     ax.set_xlabel("State Code")
     ax.set_ylabel("Total Workers")
     ax.tick_params(axis='x', rotation=45)
@@ -105,138 +102,287 @@ st.title(":bar_chart:  Industrial Human Resouces Geo-visualization")
 st.markdown("<style>div.block-container{padding-top:3rem;}</style>", unsafe_allow_html= True)
 
 with st.sidebar:
-    select = option_menu("Main menu", ["Home","Data Exploration", "Visualization"])
+    select = option_menu("Main menu", ["Intro","Data Exploration", "Visualization"])
 
-if select == "Home":
-    pass
+if select == "Intro":
 
+    col1, col2, col3 = st.columns([14,2,8])
+    with col1:
+        st.write("\n")
+        st.markdown('#### :blue[_Problem Statement_ :] In India, the industrial classification of the workforce is essential to understand the distribution of the labor force across various sectors.')
+        st.markdown('#### The classification of main workers and marginal workers, other than cultivators and agricultural laborers, by sex and by section, division, and class, has been traditionally used to understand the economic status and employment trends in thecountry.')
+        st.markdown('#### However, the current data on this classification is outdated and may not accurately reflect the current state of the workforce.')
+        st.markdown('#### The aim of this study is to update the information on the industrial classification of the main and marginal workers, other than cultivators and agricultural laborers, by sex and by section, division, and class, to provide relevant and accurate data for policy making and employment planning.')
+        st.markdown("#### :blue[_Skills take away_ :] Python scripting, Data Pre-processing, EDA, Visualization, Streamlit, Machine Learning (RandomForestClassifier), NLP.")
+        st.markdown("#### :blue[_Domain_:] Resource Management")
 
+    with col3:
+        st.header(":red[_Tools Used_ :]")
+        col1, col2 = st.columns(2)
+        with col1:
+            st.image("NumPy_logo_2020.svg.png")
+            st.image("Pandas_logo.svg.png")
+            st.image("Plotly.jpg")
+
+        with col2:
+            st.image("Matplotlib.png")
+            st.image("Scikit-learn.png")
+
+    st.markdown("#### :blue[_Outcome_:]") 
+    st.markdown("#### In this project, the population of workers for each state and its districts is analysed to provide relevant and accurate data for policy making and employment planning. Based on their work, they were catogorised based on their work nature into 7 groups using NLP, And then trained model for future usage on predicting their industry w.r.t their work nature.")
+    
     
 elif select == "Data Exploration":
-    tab1, tab2 = st.tabs(['Prediction', 'Analysis'])
+    
+    tab1, tab2 = st.tabs(['Analysis', 'Prediction'])
     
     with tab1:
-        st.header("Predict Industry Group")
-        
-        col1, col2, col3 = st.columns(3)
-        with col1:
-            state_code = st.selectbox("Select State", nlp_df['state_code'].unique())
-        with col2:
-            district_code = st.selectbox("Select District", nlp_df['district_code'].unique())
-        with col3:
-            division = st.selectbox("Select Division", nlp_df['division'].unique())
-        with col1:
-            group = st.selectbox("Select Group", nlp_df['group'].unique())
-        with col2:
-            class_ = st.selectbox("Select Class", nlp_df['class'].unique())
 
-            st.write("")
-            if st.button("Predict Industry Group"):
-                input_data = preprocess_input(state_code, district_code, division, group, class_)
-                prediction = I_HR_model.predict(input_data)[0]
+        st.header("Analysis of workers population")
 
-                st.write(f"Predicted Industry Group: {prediction}")
-
-                st.sidebar.header("Industry Group Names")
-                st.sidebar.write("0 - Non Agricultural activities")
-                st.sidebar.write("1 - Heavy Industries")
-                st.sidebar.write("2 - Govrnment service sector")
-                st.sidebar.write("3 - Small Industries")
-                st.sidebar.write("4 - Retail stores")
-                st.sidebar.write("5 - Service Organizations")
-                st.sidebar.write("6 - Wholesale stores")
-
-
-    with tab2:
-        # Distribution based on Industry group and State 
         col1, col2, = st.columns(2)
         with col1:
-            industry = st.selectbox("Select Industry,", nlp_df['industry_group'].unique())
-        
+            industry = st.selectbox(
+                                "Select Industry,", 
+                                ['Select any Industry Group'] + list(nlp_df['industry_group'].unique()))
+                            
+        industry_data = nlp_df[(nlp_df['industry_group'] == industry)]
+
         with col2:
-            state = st.selectbox("Select State,", nlp_df['state_code'].unique())
+            state = st.selectbox("Select State,",
+                                    ['Select any State'] + list(industry_data['state_code'].unique()))
         
-        filtered_data = nlp_df[(nlp_df['industry_group'] == industry) & (nlp_df['state_code'] == state)]
+        state_data = industry_data[(industry_data['state_code'] == state)]
 
 
         col1, col2 = st.columns(2)
         with col1:
-            district = st.selectbox("Select District,", filtered_data['district_code'].unique())
+            district = st.selectbox("Select District,",
+                                    ['Select any District'] + list(state_data['district_code'].unique()))
         
-        district_data = filtered_data[filtered_data['district_code'] == district]
+        district_data = state_data[state_data['district_code'] == district]
 
 
         with col2:
-            division = st.selectbox("Select Division,", district_data['division'].unique())
+            division = st.selectbox("Select Division,",
+                                    ['Select any Division'] + list(district_data['division'].unique()))
         
         division_data = district_data[district_data['division'] == division]
 
 
         col1, col2 = st.columns(2)
         with col1:
-            group = st.selectbox("Select Group,", division_data['group'].unique())
+            group = st.selectbox("Select Group,",
+                                    ['Select any Group'] + list(division_data['group'].unique()))
         
         group_data = division_data[division_data['group'] == group]
 
 
         with col2:
-            class_details = st.selectbox("Select Class,", group_data['class'].unique())
+            class_details = st.selectbox("Select Class,",
+                                            ['Select any Class'] + list(group_data['class'].unique()))
         
         class_data = group_data[group_data['class'] == class_details]
 
-        
-        st.write("")
-        st.write(f"Workers in {industry}")
+        if not class_data.empty:
+            st.write("")
+            st.write(f"Workers in {industry}")
+        else:
+            st.write("")
+
+            
 
         # Visualization of Total Workers Analysis
 
         total_fig = px.bar(class_data, x='total_workers', y='industry_group',
-                            title="Total Workers Distribution")
+                            title="Total Workers")
         st.plotly_chart(total_fig)
+
+        col1, col2 = st.columns(2)
+        # Visualization of Total Main Workers Analysis
+        with col1:
+        
+            total_main_workers_fig = px.bar(class_data, x='main_workers_total_persons', y='industry_group',
+                                title="Main Workers")
+            st.plotly_chart(total_main_workers_fig)
+
+        # Visualization of Total Marginal Workers Analysis
+        with col2:
+            
+            total_marginal_workers_fig = px.bar(class_data, x='marginal_workers_total_persons', y='industry_group',
+                                title="Marginal Workers")
+            st.plotly_chart(total_marginal_workers_fig)
+
 
         # Visualization of Male Workers Analysis
 
         col1, col2 = st.columns(2)
         with col1:
             males_fig = px.bar(class_data, x='total_male_workers', y='industry_group', 
-                               title="Total Male Workers Distribution")
+                                title="Male Workers")
             st.plotly_chart(males_fig)
+
 
         # Visualization of Female Workers Analysis
 
         with col2:
             females_fig = px.bar(class_data, x='total_female_workers', y='industry_group',
-                                  title="Total Female Workers Distribution")
+                                    title="Female Workers")
             st.plotly_chart(females_fig)
+
+    with tab2:
+        st.header("Predict Industry Group")
+        try:
+            col1, col2, col3 = st.columns(3)
+            with col1:
+                state_code = st.text_input("Enter State Code")
+            with col2:
+                district_code = st.text_input("Enter District Code")
+            with col3:
+                division = st.text_input("Enter Division")
+                
+            col1, col2, col3, col4, col5 = st.columns([2,6,2.5,6,2])
+            with col2:
+                group = st.text_input("Enter Group")
+            with col4:
+                class_ = st.text_input("Enter Class")
+                
+            col1, col2, col3, col4, col5 = st.columns([2,6,2,6,2])
+            with col2:
+                st.write("")
+                if st.button("Predict Industry Group", icon= '🔍'):
+                    input_data = preprocess_input(state_code, district_code, division, group, class_)
+                    prediction = I_HR_model.predict(input_data)[0]
+
+                    if prediction == 0:
+                        st.write(f"Predicted Industry Group : Developmental Projects")
+                    elif prediction == 1:
+                        st.write(f"Predicted Industry Group : Education")
+                    elif prediction == 2:
+                        st.write(f"Predicted Industry Group : Health sector")
+                    elif prediction == 3:
+                        st.write(f"Predicted Industry Group : Heavy Industries")
+                    elif prediction == 4:
+                        st.write(f"Predicted Industry Group : Organizational works")
+                    elif prediction == 5:
+                        st.write(f"Predicted Industry Group : Retail Stores")
+                    elif prediction == 6:
+                        st.write(f"Predicted Industry Group : Small Industries")
+
+        except:
+            st.warning('Enter the Details')
 
 
 
 elif select == "Visualization":
-    col1, col2 = st.columns(2)
-    with col1:
-        st.write("Top Industries by Main Worker Population")
-        plot_top_industries(nlp_df, 'nic_name', 'main_workers_total_persons', 20, "Top Industries by Main Worker Population", 'purple')
+
+    col1, col2, col3 = st.columns([1,5,3])
     with col2:
-        st.write("Top Industries by Marginal Worker Population")
-        plot_top_industries(nlp_df, 'nic_name', 'marginal_workers_total_persons', 20, "Top Industries by Marginal Worker Population", 'blue')
+        plot_workers_distribution_state(nlp_df)
     
-    st.write("")
-    st.write("")
+    with col3:
+        st.dataframe(state_name_code)
 
     col1, col2 = st.columns(2)
     with col1:
-        st.write("Top Industries by Total Worker Population")
-        plot_top_industries(nlp_df, 'nic_name', 'total_workers', 20, "Top Industries by Total Worker Population", 'red')
+        state = st.sidebar.selectbox("Select State,",
+                                ['Select any State'] + list( nlp_df['state_code'].unique()))
     
-    with col2:
-        st.write("Workers Distribution by States")
-        plot_workers_distribution(nlp_df)
+    state_data = nlp_df[(nlp_df['state_code'] == state)]
 
-    col1,col2 = st.columns(2)
-    with col1:
-        st.write("Rural & Urban Workers Distribution")
-        plot_rural_urban_distribution(nlp_df)
     with col2:
-        st.write("Men & Women Workers Distribution")
-        plot_men_women_distribution(nlp_df)
+        district = st.sidebar.selectbox("Select District,",
+                                ['Select any District'] + list(state_data['district_code'].unique()))
+    
+    district_data = state_data[state_data['district_code'] == district]
+
+    if state_data.empty:
+        st.info("Select State")
+    
+    
+    elif not state_data.empty:
+
+        if  district_data.empty:
+        
+            st.write("")
+            st.write("")
+
+            st.header("Visualization of Top industries in State")
+
+            col1, col2 = st.columns(2)        
+            with col1:
+                st.write("Top Industries by Main Worker Population")
+                plot_top_industries(state_data, 'nic_name', 'main_workers_total_persons', 20, "Top Industries by Main Worker Population", 'purple')
+            
+            with col2:
+                st.write("Top Industries by Marginal Worker Population")
+                plot_top_industries(state_data, 'nic_name', 'marginal_workers_total_persons', 20, "Top Industries by Marginal Worker Population", 'blue')
+            
+            st.write("")
+            st.write("")
+
+            col1, col2, col3 = st.columns([2,6,2])
+            with col2:
+                st.write("Top Industries by Total Worker Population")
+                plot_top_industries(state_data, 'nic_name', 'total_workers', 20, "Top Industries by Total Worker Population", 'red')
+            
+            st.write("")
+            st.write("")
+
+            col1,col2 = st.columns(2)
+            with col1:
+                st.write("Rural & Urban Workers Distribution")
+                plot_rural_urban_distribution(state_data)
+            
+            with col2:
+                st.write("Men & Women Workers Distribution")
+                plot_men_women_distribution(state_data)
+
+
+        elif not district_data.empty:
+
+            st.write("")
+            st.write("")
+
+            st.header("Visualization of Top industries in District")
+
+            col1, col2 = st.columns(2)        
+            with col1:
+                st.write("Top Industries by Main Worker Population")
+                plot_top_industries(district_data, 'nic_name', 'main_workers_total_persons', 20, "Top Industries by Main Worker Population", 'purple')
+            
+            with col2:
+                st.write("Top Industries by Marginal Worker Population")
+                plot_top_industries(district_data, 'nic_name', 'marginal_workers_total_persons', 20, "Top Industries by Marginal Worker Population", 'blue')
+            
+            st.write("")
+            st.write("")
+
+            col1, col2, col3 = st.columns([2,6,2])
+            with col2:
+                st.write("Top Industries by Total Worker Population")
+                plot_top_industries(district_data, 'nic_name', 'total_workers', 20, "Top Industries by Total Worker Population", 'red')
+            
+            st.write("")
+            st.write("")
+
+            col1,col2 = st.columns(2)
+            with col1:
+                st.write("Rural & Urban Workers Distribution")
+                plot_rural_urban_distribution(district_data)
+            
+            with col2:
+                st.write("Men & Women Workers Distribution")
+                plot_men_women_distribution(district_data)
+
+        else:
+            st.info("Select State and District for Further Visualization")
+
+        
+
+
+
+
+
+        
 
